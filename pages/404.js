2803fd82530963e781945b163d1553feffcbbfb6
ie97 <iwan.ie.id@gmail.com>
2021-08-25 14:44:50 +0700

    final

---
+++ pages/404.js
@@ -0,0 +1,23 @@
+import { useRouter } from "next/dist/client/router";
+import Link from "next/link"
+import { useEffect } from "react";
+
+const NotFound = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/")
+    }, 3000);
+    
+  }, [])
+
+  return (
+    <div className="not-found">
+      <h1>Ooooops...</h1>
+      <h2>That page cannot be found</h2>
+      <p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
+    </div>
+  );
+}
+ 
+export default NotFound;
---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+    
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem qui tempora necessitatibus dicta voluptates culpa a eos libero eum, corrupti repellendus laudantium cupiditate ea ex illo assumenda quas? Repudiandae, laudantium?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas suscipit explicabo repudiandae voluptates aut illum rem nobis dolores dolore commodi rerum eius nesciunt distinctio cumque iusto harum neque, optio ipsam.</p>
+      </div>
+    </>  
+  );
+}
+ 
+export default About;
